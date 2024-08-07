712016c941eee91afd6407421c15c4eb300ae726
Slava <slava@mail.ru>
2024-08-07 19:16:50 +0700

    version12

--- src/axios.js
+++ src/axios.js
@@ -3,8 +3,7 @@ import axios from "axios";
 //process.env.REACT_APP_API_URL
 
 const instance = axios.create({
-  baseURL: "http://31.128.47.84:4444/",
-  withCredentials: true,
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 instance.interceptors.request.use((config) => {
